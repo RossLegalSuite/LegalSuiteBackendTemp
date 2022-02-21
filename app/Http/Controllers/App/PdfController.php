29487be1707bfb5792410ec91d7a162f0d2e8127
Shift <shift@laravelshift.com>
2022-02-21 14:53:17 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the adopted coding style by adding a [PHP CS Fixer][1] or [PHP CodeSniffer][2] config to your project root. Feel free to use [Shift's Laravel ruleset][3] to help you get started.
    
    For more information on customizing the code style applied by Shift, [watch this short video][4].
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://github.com/squizlabs/PHP_CodeSniffer
    [3]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200
    [4]: https://laravelshift.com/videos/shift-code-style

--- app/Http/Controllers/App/PdfController.php
+++ app/Http/Controllers/App/PdfController.php
@@ -2,30 +2,25 @@
 
 namespace App\Http\Controllers\App;
 
-use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Storage;
 use Barryvdh\Snappy\Facades\SnappyPdf;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use mikehaertl\pdftk\Pdf;
 
 // Using php-pdftk (https://github.com/mikehaertl/php-pdftk)
 // Note: Pdftk binary must be installed and in the web server path
 // See: https://www.pdflabs.com/docs/install-pdftk-on-redhat-or-centos/
 
-class PdfController extends Controller {
-
-    
-
+class PdfController extends Controller
+{
     private function generateHtml($html)
-
-
     {
 
         // Fix bug in SnappyPdf::loadHTML - an <hr> in the Html causes images to not render correctly (for some strange reason)
-        $html = str_replace('<hr>', '<div style="border: 1px solid darkgrey; margin: 5px 0px"></div>', $html);        
+        $html = str_replace('<hr>', '<div style="border: 1px solid darkgrey; margin: 5px 0px"></div>', $html);
 
         $url = config('cloud.cdnUrl');
-        
+
         return <<<EOT
         <!DOCTYPE html>
         <html lang="en" class="no-js">
@@ -38,181 +33,162 @@ class PdfController extends Controller {
         </body>
         </html>        
 EOT;
-
     }
 
     public function htmlToPdf(Request $request)
     {
-
         try {
-
             $returnData = new \stdClass();
 
-            $relativePath = strtolower( session('companyCode')) . '/' . session('employeeId') . '/' . $request->folder;
-            $realPath =  storage_path('app/public') . '/' . $relativePath;
+            $relativePath = strtolower(session('companyCode')).'/'.session('employeeId').'/'.$request->folder;
+            $realPath = storage_path('app/public').'/'.$relativePath;
 
             $cloudStorage = Storage::disk(session('region'));
             $localStorage = Storage::disk('public');
 
-            $saveAs = $realPath . '/' . $request->fileName;
+            $saveAs = $realPath.'/'.$request->fileName;
 
             /*Need these php.ini settings
             upload_max_filesize = 64M
             post_max_size = 64M
-            max_execution_time = 300*/        
+            max_execution_time = 300*/
 
             SnappyPdf::setTimeOut(300);
 
-            $pdf = SnappyPdf::loadHTML( $this->generateHtml($request->bodyHtml) );
+            $pdf = SnappyPdf::loadHTML($this->generateHtml($request->bodyHtml));
 
             $pdf->setPaper($request->paperSize, $request->orientation);
 
-            if ( isset($request->headerHtml) ) $pdf->setOption('header-html', $this->generateHtml($request->headerHtml) );
-            if ( isset($request->footerHtml) ) $pdf->setOption('footer-html', $this->generateHtml($request->footerHtml) );
+            if (isset($request->headerHtml)) {
+                $pdf->setOption('header-html', $this->generateHtml($request->headerHtml));
+            }
+            if (isset($request->footerHtml)) {
+                $pdf->setOption('footer-html', $this->generateHtml($request->footerHtml));
+            }
 
-            if ( isset($request->topmargin) ) $pdf->setOption('margin-top', $request->topmargin);
-            if ( isset($request->bottommargin) ) $pdf->setOption('margin-bottom', $request->bottommargin);
-            if ( isset($request->leftmargin) ) $pdf->setOption('margin-left', $request->leftmargin);
-            if ( isset($request->rightmargin) ) $pdf->setOption('margin-right', $request->rightmargin);
-            
+            if (isset($request->topmargin)) {
+                $pdf->setOption('margin-top', $request->topmargin);
+            }
+            if (isset($request->bottommargin)) {
+                $pdf->setOption('margin-bottom', $request->bottommargin);
+            }
+            if (isset($request->leftmargin)) {
+                $pdf->setOption('margin-left', $request->leftmargin);
+            }
+            if (isset($request->rightmargin)) {
+                $pdf->setOption('margin-right', $request->rightmargin);
+            }
+
             $pdf->save($saveAs, true);
 
-            if ( isset($request->password) && isset($request->allow) ) {
-
+            if (isset($request->password) && isset($request->allow)) {
                 $pdfToolkit = new Pdf($saveAs);
 
                 $pdfToolkit->setPassword($request->password);
 
                 $pdfToolkit->allow($request->allow);
 
-                if ( !$pdfToolkit->saveAs($saveAs) ) {
-
+                if (! $pdfToolkit->saveAs($saveAs)) {
                     $returnData->error = $pdfToolkit->getError();
-
                 }
             }
 
-            if ( !isset($returnData->error) ) {
+            if (! isset($returnData->error)) {
+                $cloudStorage->put($relativePath.'/'.$request->fileName, $localStorage->get($relativePath.'/'.$request->fileName), 'public');
 
-                $cloudStorage->put($relativePath . '/' . $request->fileName, $localStorage->get($relativePath. '/' . $request->fileName), 'public');
-                    
                 $returnData->path = $relativePath;
 
-                $returnData->url = $cloudStorage->url($relativePath . '/' . $request->fileName);
+                $returnData->url = $cloudStorage->url($relativePath.'/'.$request->fileName);
 
-                $localStorage->delete($relativePath . '/' . $request->fileName);
-
+                $localStorage->delete($relativePath.'/'.$request->fileName);
             }
 
             return json_encode($returnData);
-
         } catch (\Exception $e) {
-
             $returnData->error = $e->getMessage();
 
             return json_encode($returnData);
         }
-
     }
-
 
     public function combine(Request $request)
     {
-
         $returnData = new \stdClass();
 
         $returnData->error = null;
 
         $files = [];
-        
-        $relativePath = strtolower( session('companyCode')) . '/' . session('employeeId') . '/' . $request->folder;
-        $realPath =  storage_path('app/public') . '/' . $relativePath;
+
+        $relativePath = strtolower(session('companyCode')).'/'.session('employeeId').'/'.$request->folder;
+        $realPath = storage_path('app/public').'/'.$relativePath;
 
         $cloudStorage = Storage::disk(session('region'));
         $localStorage = Storage::disk('public');
 
         $counter = -1;
 
-        foreach(json_decode($request->pdfFiles) as $file) {
-
+        foreach (json_decode($request->pdfFiles) as $file) {
             $counter++;
 
             $handle = $this->getHandle($counter);
 
             // Download a copy of the file to the local public storage directory (so pdftk can comine them)
-            $localStorage->put($relativePath . '/' . $file->fileName, $cloudStorage->get($file->path . '/' . $file->fileName));
+            $localStorage->put($relativePath.'/'.$file->fileName, $cloudStorage->get($file->path.'/'.$file->fileName));
 
             // Add to the array of files to br combined
-            $files[$handle] = storage_path('app/public') . '/' .  $relativePath . '/' . $file->fileName;
-
+            $files[$handle] = storage_path('app/public').'/'.$relativePath.'/'.$file->fileName;
         }
 
         $pdfToolkit = new Pdf($files);
 
         // Cat (Concatenate) the PDF's
-        foreach($files as $letter => $fileName) {
-
-            if ( !$pdfToolkit->cat($letter) ) $returnData->error = $pdfToolkit->getError();
-        
+        foreach ($files as $letter => $fileName) {
+            if (! $pdfToolkit->cat($letter)) {
+                $returnData->error = $pdfToolkit->getError();
+            }
         }
 
-        if ( !isset($returnData->error) ) {
-
-            if ( !$pdfToolkit->saveAs($realPath . '/' . $request->saveAs) ) {
-
+        if (! isset($returnData->error)) {
+            if (! $pdfToolkit->saveAs($realPath.'/'.$request->saveAs)) {
                 $returnData->error = $pdfToolkit->getError();
-
             } else {
-                
-                $cloudStorage->put($relativePath . '/' . $request->saveAs, $localStorage->get($relativePath. '/' . $request->saveAs), 'public');
-                
+                $cloudStorage->put($relativePath.'/'.$request->saveAs, $localStorage->get($relativePath.'/'.$request->saveAs), 'public');
+
                 $returnData->path = $relativePath;
 
-                $returnData->url = $cloudStorage->url($relativePath . '/' . $request->saveAs);
+                $returnData->url = $cloudStorage->url($relativePath.'/'.$request->saveAs);
 
-                // Delete the local files 
-                foreach(json_decode($request->pdfFiles) as $file) {
-
-                    $localStorage->delete($relativePath . '/' . $file->fileName);
-
+                // Delete the local files
+                foreach (json_decode($request->pdfFiles) as $file) {
+                    $localStorage->delete($relativePath.'/'.$file->fileName);
                 }
 
-                $localStorage->delete($relativePath . '/' . $request->saveAs);
-
+                $localStorage->delete($relativePath.'/'.$request->saveAs);
             }
-
         }
-        
+
         return json_encode($returnData);
     }
 
     private function getHandle($counter)
     {
-
         $letters = range('A', 'Z'); //['A','B' ... 'Z']
         $returnData = '';
         $chunks = 0;
-        
-        if ($counter <= 25 ) {
-        
+
+        if ($counter <= 25) {
             $returnData = $letters[$counter];
-        
         } else {
-        
-            $chunks = intdiv($counter,25) + 1;
-            
-            $letter =  ($counter % 25) - 1;
-        
-            for ($i=0; $i < $chunks; $i++) { 
-                
+            $chunks = intdiv($counter, 25) + 1;
+
+            $letter = ($counter % 25) - 1;
+
+            for ($i = 0; $i < $chunks; $i++) {
                 $returnData .= $letters[$letter];
-        
             }
-        
         }
 
         return $returnData;
-
     }
 
     // public function setPassword(Request $request)
@@ -245,6 +221,4 @@ EOT;
         FillIn
         AllFeatures – Allows the user to perform all of the above, and top quality printing.
        ******************************************/
-
-
 }

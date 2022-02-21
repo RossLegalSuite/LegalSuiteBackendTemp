acc699183f860dd6362537e0e1fa5e0c6826978f
Shift <shift@laravelshift.com>
2022-02-21 14:45:46 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the adopted coding style by adding a [PHP CS Fixer][1] or [PHP CodeSniffer][2] config to your project root. Feel free to use [Shift's Laravel ruleset][3] to help you get started.
    
    For more information on customizing the code style applied by Shift, [watch this short video][4].
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://github.com/squizlabs/PHP_CodeSniffer
    [3]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200
    [4]: https://laravelshift.com/videos/shift-code-style

--- app/Http/Controllers/App/GenericTableController.php
+++ app/Http/Controllers/App/GenericTableController.php
@@ -3,44 +3,46 @@
 namespace App\Http\Controllers\App;
 
 use App\Custom\DataTablesHelper;
-use Illuminate\Http\Request;
 use App\Custom\Utils;
+use Illuminate\Http\Request;
 
-class GenericTableController extends Controller {
-
+class GenericTableController extends Controller
+{
     public function get(Request $request, $tableName, $id = null)
     {
-
         try {
 
             // If these are sent in the request
-            if ( isset($request->tableName) ) $tableName = $request->tableName;
-            if ( isset($request->recordid) ) $id = $request->recordid;
+            if (isset($request->tableName)) {
+                $tableName = $request->tableName;
+            }
+            if (isset($request->recordid)) {
+                $id = $request->recordid;
+            }
 
-            $apiUrl = "/" . strtolower( $tableName) . "/get";
+            $apiUrl = '/'.strtolower($tableName).'/get';
 
-            if ( isset($id)) $apiUrl .= "/" . $id;
+            if (isset($id)) {
+                $apiUrl .= '/'.$id;
+            }
 
             $postFields = http_build_query($request->all());
 
-            $response = Utils::SetCurlParams($apiUrl,'POST', $postFields);
+            $response = Utils::SetCurlParams($apiUrl, 'POST', $postFields);
 
             return json_encode($response);
-    
-        } catch(\Exception $e)  {
-    
+        } catch (\Exception $e) {
             $returnData['errors'] = $e->getMessage();
-            return json_encode($returnData);
-    
-        }
 
+            return json_encode($returnData);
+        }
     }
 
-    public function store(Request $request, $tableName) {
+    public function store(Request $request, $tableName)
+    {
+        $apiUrl = '/'.strtolower($tableName).'/';
 
-        $apiUrl = "/" . strtolower( $tableName) . "/";
-
-        if ( isset($request->recordid)) {
+        if (isset($request->recordid)) {
             $apiUrl .= 'update';
         } else {
             $apiUrl .= 'store';
@@ -48,127 +50,95 @@ class GenericTableController extends Controller {
 
         $postFields = http_build_query($request->all());
 
-        $response = Utils::SetCurlParams($apiUrl,'POST', $postFields);
+        $response = Utils::SetCurlParams($apiUrl, 'POST', $postFields);
 
         return json_encode($response);
-
     }
 
-    public function update(Request $request, $tableName) {
-
-        $apiUrl = "/" . strtolower( $tableName) . "/update";
+    public function update(Request $request, $tableName)
+    {
+        $apiUrl = '/'.strtolower($tableName).'/update';
 
         $postFields = http_build_query($request->all());
 
-        $response = Utils::SetCurlParams($apiUrl,'POST', $postFields);
+        $response = Utils::SetCurlParams($apiUrl, 'POST', $postFields);
 
         return json_encode($response);
-
     }
 
     public function getTemplateData(Request $request, $tableName, $id)
     {
-
         try {
-
-
-            $apiUrl = '/' . strtolower( $tableName) . '/gettemplatedata/' . $id ;
+            $apiUrl = '/'.strtolower($tableName).'/gettemplatedata/'.$id;
 
             $postFields = http_build_query($request->all());
 
-            $response = Utils::SetCurlParams($apiUrl,'POST', $postFields);
+            $response = Utils::SetCurlParams($apiUrl, 'POST', $postFields);
 
             return json_encode($response);
-    
-        } catch(\Exception $e)  {
-    
+        } catch (\Exception $e) {
             $returnData['errors'] = $e->getMessage();
-            return json_encode($returnData);
-    
-        }
 
+            return json_encode($returnData);
+        }
     }
 
     public function first(Request $request, $tableName)
     {
-
         try {
-
-            $apiUrl = '/' . strtolower( $tableName) . '/first' ;
+            $apiUrl = '/'.strtolower($tableName).'/first';
 
             $postFields = http_build_query($request->all());
 
-            $response = Utils::SetCurlParams($apiUrl,'POST', $postFields);
+            $response = Utils::SetCurlParams($apiUrl, 'POST', $postFields);
 
             return json_encode($response);
-    
-        } catch(\Exception $e)  {
-    
+        } catch (\Exception $e) {
             $returnData['errors'] = $e->getMessage();
+
             return json_encode($returnData);
-    
         }
-
     }
 
+    public function destroy(Request $request)
+    {
+        $apiUrl = '/'.strtolower($request->tableName).'/';
 
-
-
-    public function destroy(Request $request) {
-
-        $apiUrl = "/" . strtolower( $request->tableName) . "/";
         return DataTablesHelper::destroy($request->id, $apiUrl);
-
     }
-
 
     public function destroy_verbose(Request $request)
     {
-
         $returnData = new \stdClass();
 
         if (is_array($request->id)) {
-
             foreach ($request->id as $id) {
-
                 try {
-
                     $apiUrl = "/{$request->tableName}/{$id}";
-                    $customRequest = 'DELETE'; 
-            
-                    $response = Utils::SetCurlParams($apiUrl,$customRequest);
+                    $customRequest = 'DELETE';
 
-                } catch(\Exception $e)  {
-            
+                    $response = Utils::SetCurlParams($apiUrl, $customRequest);
+                } catch (\Exception $e) {
                     $returnData->errors = $e->getMessage();
-                    return json_encode($returnData);
 
-                }       
-    
+                    return json_encode($returnData);
+                }
             }
 
-            return json_encode($returnData);            
-
-        } else {  
-
+            return json_encode($returnData);
+        } else {
             try {
-
                 $apiUrl = "/{$request->tableName}/{$request->id}";
-                $customRequest = 'DELETE'; 
-        
-                $response = Utils::SetCurlParams($apiUrl,$customRequest);
-                
+                $customRequest = 'DELETE';
+
+                $response = Utils::SetCurlParams($apiUrl, $customRequest);
+
                 return json_encode($response);
-        
-            } catch(\Exception $e)  {
-        
+            } catch (\Exception $e) {
                 $returnData->errors = $e->getMessage();
+
                 return json_encode($returnData);
-        
-            }  
-            
+            }
         }
     }
-
-
 }
